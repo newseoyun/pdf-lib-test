fd1c1a6fa2c673b73a0d5e3a3216c3feb130ed1c
newseoyun <newseoyun@gmail.com>
2021-06-20 22:54:00 +0900

    .

---
+++ test.ts
@@ -0,0 +1,24 @@
+const { PDFDocument } = require('pdf-lib');
+const fs = require('fs');
+
+run().catch(err => console.log(err));
+
+async function run() {
+  // Create a new document and add a new page
+  const doc = await PDFDocument.create();
+  const page = doc.addPage();
+
+  // Load the image and store it as a Node.js buffer in memory
+  let img = fs.readFileSync('./resources/neo1.png');
+  img = await doc.embedPng(img);
+
+  // Draw the image on the center of the page
+  const { width, height } = img.scale(1);
+  page.drawImage(img, {
+    x: page.getWidth() / 2 - width / 2,
+    y: page.getHeight() / 2 - height / 2
+  });
+
+  // Write the PDF to a file
+  fs.writeFileSync('./test.pdf', await doc.save());
+}
